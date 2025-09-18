8aba2cc8d2e45b9966088afe6d7d7c14c46af373
maham904 <mahamkhanlu@gmail.com>
2025-09-19 00:34:48 +0100

    test cases added issue fixed

--- shop/tests.py
+++ shop/tests.py
@@ -1,23 +1,21 @@
-import os
 from django.test import TestCase
 from django.urls import reverse
-from decimal import Decimal
-from django.core.files import File
 from shop.models import Product
-from django.conf import settings  # <- important
+from django.core.files.uploadedfile import SimpleUploadedFile
+from decimal import Decimal
 
 class ProductViewTest(TestCase):
     def setUp(self):
-        # Correct path using BASE_DIR
-        image_path = os.path.join(settings.BASE_DIR, 'media', 'lipsticks', 'lipstik.png')
-        with open(image_path, 'rb') as f:
-            product_image = File(f)
-            Product.objects.create(
-                name="Lipstick Pink",
-                description="Soft pink shade",
-                price=Decimal('12.50'),
-                image=product_image
-            )
+        # Create an in-memory fake image
+        image_content = b'\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x80\x00\x00\x00\x00\x00\xFF\xFF\xFF\x21\xF9\x04\x00\x00\x00\x00\x00\x2C\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02\x4C\x01\x00\x3B'
+        image = SimpleUploadedFile("lipstik.png", image_content, content_type="image/png")
+
+        Product.objects.create(
+            name="Lipstick Pink",
+            description="Soft pink shade",
+            price=Decimal('12.50'),
+            image=image
+        )
 
     def test_homepage_loads(self):
         response = self.client.get(reverse("product_list"))
